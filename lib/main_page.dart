0c9c3bba290c228d4def3e0f41b9635b43c9fd6b
SEI4X <mashkov0599@gmail.com>
2020-10-24 16:07:37 +0300

    Added search bar

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:join_party/map_modules/search_bar.dart';
 import 'map_modules/map_page.dart';
 
 class MainPage extends StatefulWidget {
@@ -71,7 +72,14 @@ class _MainPageState extends State<MainPage> {
                     ),
                   ),
                   bottomIcons == BottomIcons.Map
-                      ? Icon(EvaIcons.searchOutline, color: Colors.blueGrey)
+                      ? IconButton(
+                          icon: Icon(EvaIcons.searchOutline),
+                          color: Colors.blueGrey,
+                          onPressed: () {
+                            showSearch(
+                                context: context, delegate: SearchData());
+                          },
+                        )
                       : bottomIcons == BottomIcons.Events
                           ? Container(width: 30)
                           : bottomIcons == BottomIcons.Chats
